2922b836ab44b5a79ca4c166fbcfcda418b6aa54
varungrover001 <94754512+varungrover001@users.noreply.github.com>
2025-07-24 14:57:15 +0530

    Executing Shell Script

---
+++ .github/workflows/ascii-script.sh
@@ -0,0 +1,7 @@
+#/bin/sh
+
+sudo apt-get install cowsay -y
+run: cowsay -f dragon "Run for cover, I am a Dragon... RAWR" >> dragon.txt
+run: grep -i "dragon" dragon.txt
+run: cat dragon.txt
+ls -ltra
